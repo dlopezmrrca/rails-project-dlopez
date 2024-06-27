030e9093f323b7f6212c35527c38e2bc906d10df
Daniel <dlopezmaldonado@rrc.ca>
2024-06-27 16:05:59 -0500

    hirarcgucak searhing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,28 +4,32 @@ class HomeController < ApplicationController
   end
 
   def about
-
   end
 
   def characters
-    @characters = Character.all
     @characters = Character.paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @character = Character.find(params[:id])
-    @jutsus = @character.jutsus
-    @clan_name = @character.clans.first.name if @character.clans.any?
     @jutsus = @character.jutsus.paginate(page: params[:page], per_page: 10)
-
+    @clan_name = @character.clans.first.name if @character.clans.any?
   end
 
-  def search
-    if params[:query].present?
-      @characters = Character.where('name LIKE ?', "%#{params[:query]}%").paginate(page: params[:page], per_page: 10)
-    else
-      @characters = Character.paginate(page: params[:page], per_page: 10)
-    end
+  def search_characters
+    @characters = filtered_characters.paginate(page: params[:page], per_page: 10)
+
+    render 'search'
   end
 
+  private
+
+  def filtered_characters
+    characters = Character.joins(:character_clans).where.not(character_clans: { clan_id: nil })
+
+    characters = characters.where('name LIKE ?', "%#{params[:query]}%") if params[:query].present?
+    characters = characters.where(character_clans: { clan_id: params[:clan_id] }) if params[:clan_id].present? && params[:clan_id].to_i > 0
+
+    characters.distinct
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   get 'home/characters'
   get 'home/characters/:id', to: 'home#show', as: 'home_character'
 
-  get 'home/search', to: 'home#search', as: 'search_characters'
+
+  get 'home/search_characters', to: 'home#search_characters', as: 'search_characters'
+
+
 
 
   get "up" => "rails/health#show", as: :rails_health_check
