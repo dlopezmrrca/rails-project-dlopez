8bb149c36df57b8171ff0fc2ecba5a2e8204e94b
Daniel <dlopezmaldonado@rrc.ca>
2024-06-27 10:03:08 -0500

    search complete, working on associations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @characters = Character.paginate(page: params[:page], per_page: 10)
   end
 
   def about
@@ -10,4 +11,19 @@ class HomeController < ApplicationController
     @characters = Character.all
     @characters = Character.paginate(page: params[:page], per_page: 10)
   end
+
+  def show
+    @character = Character.find(params[:id])
+    @jutsus = @character.jutsus
+    @clan = @character.clans.first
+  end
+
+  def search
+    if params[:query].present?
+      @characters = Character.where('name LIKE ?', "%#{params[:query]}%").paginate(page: params[:page], per_page: 10)
+    else
+      @characters = Character.paginate(page: params[:page], per_page: 10)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get 'home/about'
   get 'home/characters'
+  get 'home/characters/:id', to: 'home#show', as: 'home_character'
+
+  get 'home/search', to: 'home#search', as: 'search_characters'
+
 
   get "up" => "rails/health#show", as: :rails_health_check
 
